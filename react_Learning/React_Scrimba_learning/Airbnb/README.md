c52de04e15752f5c7616bf1067269ab2fe251719
Cemordi2 <emordichukwuma@gmail.com>
2023-09-04 12:30:50 +0100

    Build an Airbnb Experience clone

---
+++ react_Learning/React_Scrimba_learning/Airbnb/README.md
@@ -0,0 +1,8 @@
+#AirBnB experiences clone - 
+
+Description
+
+This project is a self-learning activities.
+
+Authors
+    Emordi Chuckwuma - Github
---
+++ react_Learning/React_Scrimba_learning/Airbnb/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Navbar from "./aa/Navbar";
+import "./App.css"
+import Hero from "./aa/Hero";
+import Card from "./aa/Card";
+
+
+
+export default function App(){
+       return(
+              <div>
+                         <Hero />
+                         <section className ="sec">
+      <Card 
+        img1 = 'list2.jpg'
+        img2 = 'list1.jpg'
+        rating = {5.0}
+        review = {6}
+        country = "USA"
+        price= {130} />
+    <Card 
+      img1 = '/Hero.jpg'
+      img2 = 'list1.jpg'
+      rating = {5.0}
+      review = {20}
+      country = "USA"
+      price = {120}
+        />
+    <Card 
+      img1 = 'all.jpg'
+      img2 = 'list1.jpg'
+      rating = {4.0}
+      review = {61}
+      country = "USA"
+      price = {112}
+      />
+ </section>
+              </div>
+       )
+}
