200484ba331aa9bd9cc9c0050094eb712ff4ab8c
Cemordi2 <emordichukwuma@gmail.com>
2024-08-13 21:13:45 +0100

    Delete react_Learning/React_Scrimba_learning/Airbnb directory

--- react_Learning/React_Scrimba_learning/Airbnb/README.md
+++
@@ -1,9 +0,0 @@
-#AirBnB experiences clone - 
-
-Description
-
-This project is a self-learning activities.
-
-
-Authors
-    Emordi Chuckwuma - Github
--- react_Learning/React_Scrimba_learning/Airbnb/src/aa/Navbar.js
+++
@@ -1,9 +0,0 @@
-import react from "react"
-
-export default function Navbar(){
-       return(
-              <nav className = "navbar">
-              <img src={`./images/Airbnb_logo.png`} alt="Airbnb Logo" className ="Logo"/>
-              </nav>
-              )
-}
